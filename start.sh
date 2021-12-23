7462d69dcdd537234edb525c8cfce16d2e2499cb
Sahanhishara500 <85470759+Sahanhishara500@users.noreply.github.com>
2021-12-23 19:18:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/sahanhisharasl/xandria.git 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Xandria
+  git clone $UPSTREAM_REPO /Xandriabboott
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
